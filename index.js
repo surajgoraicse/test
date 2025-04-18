708993d92a1c79b1fb60dbfebe88886b9575c6ec
surajgoraicse <surajgoraicse@gmail.com>
2025-04-18 06:53:10 +0530

    port update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 
 const app = express()
+const port = process.env.PORT || 10000
 app.use(express.json())
 
 app.get('/', (req, res) => {
@@ -12,6 +13,6 @@ app.post('/data', (req, res) => {
     res.status(200).json({ data: `data is ${data}` })
 })
 
-app.listen(8001, () => {
+app.listen(port, () => {
     console.log("server is listening at port http://localhost:8001");
 })
